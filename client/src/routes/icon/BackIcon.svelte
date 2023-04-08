9e63735798701204f6e83879712277cd8b930b19
bersling <bersling@gmail.com>
2023-04-08 08:08:05 +0200

    fix back button styling

--- client/src/routes/icon/BackIcon.svelte
+++ client/src/routes/icon/BackIcon.svelte
@@ -12,12 +12,12 @@
 	width={size}
 	height={size}
 	viewBox="0 0 473.654 473.654"
-	style="enable-background:new 0 0 473.654 473.654; border: 5px solid #fff; border-radius: 50%"
+	style="enable-background:new 0 0 473.654 473.654; border: 7px solid transparent; border-radius: 50%"
 	xml:space="preserve"
 >
-	<circle style="fill: hsl(0, 0%, 30%)" cx="236.827" cy="236.827" r="236.827" />
+	<circle style="fill: white;" cx="236.827" cy="236.827" r="236.827" />
 	<path
-		style="fill:#FFFFFF;"
+		style="fill:rgb(45,45,45);"
 		d="M338.465,207.969c-43.487,0-86.975,0-130.459,0c11.08-11.08,22.161-22.161,33.241-33.245
 	c25.56-25.56-14.259-65.084-39.883-39.456c-27.011,27.011-54.018,54.022-81.029,81.033c-10.841,10.841-10.549,28.907,0.213,39.669
 	c27.011,27.007,54.018,54.018,81.029,81.025c25.56,25.56,65.084-14.259,39.456-39.883c-11.013-11.013-22.026-22.026-33.039-33.035
